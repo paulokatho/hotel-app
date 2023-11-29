481166748dd4290221758990fca3bc528207b72e
paulokatho <pkatho@gmail.com>
2023-11-29 02:30:40 +0000

    feat: Criar extensão para utilizarmos o Heath Check para ajudar a aplicação a saber se a aplicação está em "pé" ou não com o Liveness Check

---
+++ src/main/java/com/katho/hotel/LivenessCheck.java
@@ -0,0 +1,13 @@
+package com.katho.hotel;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class LivenessCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Hotel - Estou vivo! - LivenessCheck");
+    }
+}
